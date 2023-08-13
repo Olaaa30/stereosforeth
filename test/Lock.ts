1671e097cdbb1644c5e9c41b201260997c89080d
Olayinka <adebisiolayinka30@gmail.com>
2023-08-13 19:19:41 +0100

    created hardhat helpers

--- test/Lock.ts
+++ test/Lock.ts
@@ -22,6 +22,7 @@ describe("Lock", function () {
     const [owner, otherAccount] = await ethers.getSigners();
 
     const Lock = await ethers.getContractFactory("Lock");
+
     const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
     return { lock, unlockTime, lockedAmount, owner, otherAccount };
@@ -70,19 +71,19 @@ describe("Lock", function () {
         );
       });
 
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
+      // it("Should revert with the right error if called from another account", async function () {
+      //   const { lock, unlockTime, otherAccount } = await loadFixture(
+      //     deployOneYearLockFixture
+      //   );
 
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
+      //   // We can increase the time in Hardhat Network
+      //   await time.increaseTo(unlockTime);
 
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
+      //   // We use lock.connect() to send a transaction from another account
+      //   await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
+      //     "You aren't the owner"
+      //   );
+      // });
 
       it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
         const { lock, unlockTime } = await loadFixture(
--- test/StereoAI.ts
+++ test/StereoAI.ts
@@ -1,5 +1,7 @@
-import { ethers } from "hardhat";
+import { developmentChains } from "../helper-hardhat-config";
 import { expect } from "chai";
+import { ethers } from "hardhat";
+import { it } from "mocha";
 
 describe("StereoAI",async () => {
     
