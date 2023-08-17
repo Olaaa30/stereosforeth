31446ca8c8fcaeb995495b8d03792db8f5175a94
Olayinka <adebisiolayinka30@gmail.com>
2023-08-17 18:40:30 +0100

    fixed conflicts

--- test/TuneBot.ts
+++ test/TuneBot.ts
@@ -6,37 +6,37 @@ import { developmentChains } from "../helper-hardhat-config";
 import { expect } from "chai";
 import { ethers, network } from "hardhat";
 import { it } from "mocha";
-import { StereoAI } from "../typechain-types/contracts/StereoAI";
+import { TuneBot } from "../typechain-types/contracts/";
 
 // * if the newwork will be hardhat or localhost then these tests will be run.
 
 !developmentChains.includes(network.name)
   ? describe.skip
-  : describe("StereoAI Contract - Unit tests", () => {
+  : describe("tuneBot Contract - Unit tests", () => {
       let deployer, otherAddress;
       let maxSupply: bigint = 1000000000n;
-      let stereoAI: StereoAI;
+      let tuneBot: tuneBot;
       const ONE_WEEK_IN_SECS: bigint = BigInt(60 * 60 * 24 * 7);
 
-      async function deployStereoAIFixture() {
+      async function deploytuneBotFixture() {
         const deployTime = await time.latest();
 
         [deployer, otherAddress] = await ethers.getSigners();
 
-        const StereoAI = await ethers.getContractFactory("StereoAI");
-        const stereoAI = await StereoAI.deploy();
+        const tuneBot = await ethers.getContractFactory("tuneBot");
+        const tuneBot = await tuneBot.deploy();
 
-        return { stereoAI, deployer, otherAddress };
+        return { tuneBot, deployer, otherAddress };
       }
       // beforeEach(async () => {
       //   if (!developmentChains.includes(network.name)) {
       //     throw new Error("You need to be on a development chain to run tests");
       //   }
       //   [deployer, otherAddress] = await ethers.getSigners();
-      //   const StereoAI = await ethers.getContractFactory("StereoAI");
-      //   const stereoAI = await StereoAI.deploy();
-      //   // stereoAI.waitForDeployment();
-      //   // const addr = stereoAI.getAddress()
+      //   const tuneBot = await ethers.getContractFactory("tuneBot");
+      //   const tuneBot = await tuneBot.deploy();
+      //   // tuneBot.waitForDeployment();
+      //   // const addr = tuneBot.getAddress()
       // });
 
       // afterEach(() => {
@@ -44,67 +44,67 @@ import { StereoAI } from "../typechain-types/contracts/StereoAI";
       // });
       describe("Deployment", function () {
         it("should mint half of the max supply", async () => {
-          const { stereoAI, deployer, otherAddress } = await loadFixture(
-            deployStereoAIFixture
+          const { tuneBot, deployer, otherAddress } = await loadFixture(
+            deploytuneBotFixture
           );
           const divider: bigint = 2n;
           const decimals: bigint = BigInt(1e18);
           const halfOfTotalSupply: bigint = maxSupply / divider;
-          const stereoAIBalance: bigint =
-            await stereoAI.getCurrentTotalSupply();
-          expect(stereoAIBalance).to.equal(halfOfTotalSupply);
+          const tuneBotBalance: bigint =
+            await tuneBot.getCurrentTotalSupply();
+          expect(tuneBotBalance).to.equal(halfOfTotalSupply);
         });
         it("should mint half of the max supply to the deployer", async () => {
-          const { stereoAI, deployer, otherAddress } = await loadFixture(
-            deployStereoAIFixture
+          const { tuneBot, deployer, otherAddress } = await loadFixture(
+            deploytuneBotFixture
           );
           const divider: bigint = 2n;
           const decimals: bigint = BigInt(1e18);
           const halfOfTotalSupply: bigint = (maxSupply / divider) * decimals;
-          const deployerBalance = await stereoAI.balanceOf(deployer.address);
+          const deployerBalance = await tuneBot.balanceOf(deployer.address);
           expect(deployerBalance).to.equal(halfOfTotalSupply);
         });
       });
       describe("Contract Functions", function () {
         it("mintAfterOneWeek - should revert when the rest of the tokens are minted too early", async () => {
-          const { stereoAI, deployer, otherAddress } = await loadFixture(
-            deployStereoAIFixture
+          const { tuneBot, deployer, otherAddress } = await loadFixture(
+            deploytuneBotFixture
           );
-          await expect(stereoAI.mintAfterOneWeek()).to.be.revertedWith(
+          await expect(tuneBot.mintAfterOneWeek()).to.be.revertedWith(
             "Minting too early, It hasn't been a week since launch"
           );
         });
         it("mintAfterFourWeeks - should revert when the rest of the tokens are minted too early", async () => {
-          const { stereoAI, deployer, otherAddress } = await loadFixture(
-            deployStereoAIFixture
+          const { tuneBot, deployer, otherAddress } = await loadFixture(
+            deploytuneBotFixture
           );
-          await expect(stereoAI.mintAfterFourWeeks()).to.be.revertedWith(
+          await expect(tuneBot.mintAfterFourWeeks()).to.be.revertedWith(
             "Minting too early, It hasn't been four weeks since launch"
           );
         });
         it("mintAfterTenSeconds",async () => {
-          const { stereoAI, deployer, otherAddress } = await loadFixture(
-            deployStereoAIFixture
+          const { tuneBot, deployer, otherAddress } = await loadFixture(
+            deploytuneBotFixture
           );
           const decimals: bigint = BigInt(1e18);
           const expectedBalance : bigint = BigInt(750000000) * decimals;
-          await stereoAI.mintAfterTenSeconds();
+          await tuneBot.mintAfterTenSeconds();
           // const deployerBalance = await ;
-          expect(await stereoAI.balanceOf(deployer.address)).to.equal(expectedBalance);
+          expect(await tuneBot.balanceOf(deployer.address)).to.equal(expectedBalance);
         })
       });
       describe("Transactions", () => {
         it("should tranfer tokens between accounts", async () => {
-          const { stereoAI, deployer, otherAddress } = await loadFixture(
-            deployStereoAIFixture
+          const { tuneBot, deployer, otherAddress } = await loadFixture(
+            deploytuneBotFixture
           );
           // Transfer 50 tokens from deployer to otherAddress
           const decimals: bigint = BigInt(1e18);
           const amount: bigint = BigInt(50);
           await expect(
-            stereoAI.transfer(otherAddress.address, amount)
+            tuneBot.transfer(otherAddress.address, amount)
           ).to.changeTokenBalances(
-            stereoAI,
+            tuneBot,
             [deployer, otherAddress],
             [-amount, amount]
           );
@@ -112,9 +112,9 @@ import { StereoAI } from "../typechain-types/contracts/StereoAI";
           // Transfer 50 tokens from otherAddress to deployer
           // We use .connect(signer) to send a transaction from another account
           await expect(
-            (stereoAI.connect(otherAddress) as StereoAI).transfer(deployer.address, amount)
+            (tuneBot.connect(otherAddress) as tuneBot).transfer(deployer.address, amount)
           ).to.changeTokenBalances(
-            stereoAI,
+            tuneBot,
             [otherAddress, deployer],
             [-amount, amount]
           );
